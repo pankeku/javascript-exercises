2edcd4df0f0d90cb8280275f25c6aeca28cf7489
pankeku <drwagw@gmail.com>
2022-05-02 19:35:41 +0300

    Solved and tested TOP JS ex. 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,25 @@
-const sumAll = function() {
+const sumAll = function(begin, end) {
+
+    let sum = 0;
+
+    if (begin < 0 || end < 0) {
+        return `ERROR`;
+    } else if (typeof begin != "number" || typeof end != "number") {
+        return `ERROR`;
+    }
+
+    if (begin > end) {
+        let temp = 0;
+        temp = begin;
+        begin = end;
+        end = temp;
+    }
+
+    for (; begin <= end; begin++) {
+        sum += begin;
+    }
+
+    return sum;
 
 };
 

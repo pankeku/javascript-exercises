a9715636a30f4524036065ad54642ba195f65f58
pankeku <drwagw@gmail.com>
2022-05-03 15:54:37 +0300

    Completed and tested

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,4 +1,21 @@
-const palindromes = function () {
+const palindromes = function (text) {
+
+// 1. nuimt kablelius ir tarpus, palikt tik raides. 
+// 2. reversint, returnint nauja stringa ir palygint su originalu
+
+    text = text.replace(/[^a-zA-Z0-9]+/g, "").toLowerCase();
+    
+
+    let reversed = text.split("").reverse().join("");
+
+    console.log(`${text}\n${reversed}`)
+
+    if (text === reversed) {
+        return true;
+    }
+
+    return false;
+    
 
 };
 

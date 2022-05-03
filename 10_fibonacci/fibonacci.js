49c61df8c89671c80ebd59f516fe4a6abcd34524
pankeku <drwagw@gmail.com>
2022-05-03 16:26:09 +0300

    Completed and tested

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+
+if (typeof number === "string") {
+        number = Number(number);
+    }
+    
+
+if (number < 0) return "OOPS";
+
+let first = 0;
+let second = 1;
+let result = 1;
+
+    for (let i = 1; i < number; i++) {
+        
+        result = first + second;
+        first = second;
+        second = result;
+
+    }
+
+    return result;
 
 };
 

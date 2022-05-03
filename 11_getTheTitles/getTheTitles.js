be14fe380d58ae7853131cd4019dddfcb69ec369
pankeku <drwagw@gmail.com>
2022-05-03 16:40:06 +0300

    Completed and tested

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(info) {
+let titles = [];
 
+for (element of info) {
+    titles.push(element.title);
+}
+
+return titles;
 };
 
 // Do not edit below this line

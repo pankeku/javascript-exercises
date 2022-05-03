1bcab08a1ecb01215d5a0552055c4fa5e52dae1f
pankeku <drwagw@gmail.com>
2022-05-03 13:38:10 +0300

    Completed and tested exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,38 @@
-const ftoc = function() {
+const ftoc = function(fahrenheit) {
+
+  let result = (fahrenheit - 32) * 0.5556;
+  console.log(`Before rounding: ${result}`);
+
+  result = roundedToFixed(result, 1);
+
+  result = parseFloat(result.toString());
+ 
+  console.log(result);
+
+  return result;
 
 };
 
-const ctof = function() {
+const ctof = function(celsius) {
+
+  let result = (celsius * 1.8) + 32;
+  console.log(`Before rounding: ${result}`);
+
+  result = roundedToFixed(result, 1);
+
+  result = parseFloat(result.toString());
+ 
+  console.log(result);
+
+  return result;
 
 };
+
+function roundedToFixed(input, digits){
+  let rounded = Math.pow(10, digits);
+  return (Math.round(input * rounded) / rounded).toFixed(digits);
+}
+
 
 // Do not edit below this line
 module.exports = {

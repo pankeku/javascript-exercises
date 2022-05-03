372193c19847454f6247ba180f911ffa3796c2f0
pankeku <drwagw@gmail.com>
2022-05-03 15:26:13 +0300

    Completed and tested

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,66 @@
-const add = function() {
+const add = function(number1, number2) {
+
+  return number1 + number2;
+  
+};
+
+const subtract = function(number1, number2) {
+	
+  return number1 - number2;
+
+};
+
+const sum = function(numbers) {
+
+let suma = 0;
+
+  for (let i = 0; i <numbers.length; i++) {
+
+    suma = suma + numbers[i];
+
+  }
+
+  return suma;
 	
 };
 
-const subtract = function() {
+const multiply = function(numbers) {
+
+  let result = 1;
+
+  for (number of numbers) {
+    result = result * number;
+  }
+
+  return result;
+
+};
+
+const power = function(number, power) {
 	
+  let result = number;
+
+  for (let i = 1; i < power; i++) {
+
+    result = result * number;
+  
+  }
+
+  return result;
+
 };
 
-const sum = function() {
-	
-};
+const factorial = function(number) {
 
-const multiply = function() {
+  let result = 1;
 
-};
+  for (let i = 1; i <= number; i++) {
 
-const power = function() {
-	
-};
+    result = result * i;
 
-const factorial = function() {
+  }
+
+  return result;
 	
 };
 

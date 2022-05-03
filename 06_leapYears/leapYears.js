cd7c623415e00983329bda8355463aeedaa24ea6
pankeku <drwagw@gmail.com>
2022-05-03 13:05:08 +0300

    Condensed solution

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,19 +1,12 @@
 const leapYears = function(year) {
 
-    if (year % 4 == 0) {
-        if (year % 100 == 0 && year % 400 != 0) {
-            console.log(`${year} - not a leap year.`);
-            return false;
-        } else {
+    if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
             console.log(`${year} - is a leap year.`);
             return true;
-        }
-    } else {
-        console.log(`${year} - not a leap year.`);
-        return false;
-    }
-
-};
+        } 
+            console.log(`${year} - not a leap year.`);
+            return false;
+}
 
 // Do not edit below this line
 module.exports = leapYears;

bf0da349c30355799f2c0240647a6212292b7010
pankeku <drwagw@gmail.com>
2022-05-03 12:47:36 +0300

    Completed exercise and tested

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,17 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+    if (year % 4 == 0) {
+        if (year % 100 == 0 && year % 400 != 0) {
+            console.log(`${year} - not a leap year.`);
+            return false;
+        } else {
+            console.log(`${year} - is a leap year.`);
+            return true;
+        }
+    } else {
+        console.log(`${year} - not a leap year.`);
+        return false;
+    }
 
 };
 
